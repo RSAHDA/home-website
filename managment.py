72622a808ff8760092d8b2436a4dfcf0542fabd0
rishitv26 <75286874+rishitv26@users.noreply.github.com>
2021-12-28 18:52:54 -0500

    making things better

--- managment.py
+++ managment.py
@@ -1,9 +1,6 @@
 import socket
 
-# this is like this for now so that it can actually validate for now.
-
-
-blocked_ipx = []
+# this is like this for now so that it can actually validate for now
 hostname = socket.gethostname()
 ips = [socket.gethostbyname(hostname)]
 
