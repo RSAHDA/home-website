c5801d7ccb62e551400748f2e1eaf78ae2a72a10
rishitv26 <75286874+rishitv26@users.noreply.github.com>
2021-12-30 14:27:08 -0500

    temporary come-back to sqlite. (use new branch to add your own database)

--- rsahda/settings.py
+++ rsahda/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'rsahda.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'rsahda',
-        'USER': 'postgres',
-        'PASSWORD': 'rsahda!@#$%^&*()',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
