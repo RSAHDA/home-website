c9e857098f858f374cbf4bf280df0d0dff6a137c
RagavN12 <sriragav@protonmail.com>
2022-01-02 13:57:22 -0500

    added superusers and changed it to sqlite3

--- rsahda/settings.py
+++ rsahda/settings.py
@@ -79,15 +79,10 @@ WSGI_APPLICATION = 'rsahda.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'rsahda',
-        'USER': 'postgres',
-        'PASSWORD': 'rsahda!@#$%^&*()',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
