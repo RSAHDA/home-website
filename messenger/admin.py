c312de393ec212ddde266eb883a7ae626505146f
RagavN12 <sriragav@protonmail.com>
2022-01-23 17:05:55 -0500

    error expected

--- messenger/admin.py
+++ messenger/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from . models import *
+from .models import *
 
 
 class MessagesAdmin(admin.ModelAdmin):
     list_display = ['message']
 
-admin.site.register(SentMail,MessagesAdmin)
+
+admin.site.register(SentMail, MessagesAdmin)
