bd74b0aafa599cca8de9336bf3b6ecd335ba809a
rishitv26 <75286874+rishitv26@users.noreply.github.com>
2022-03-25 20:03:38 -0400

    working like a mustard (adding updates) (ALMOST DONE!!!)

--- rsahda/settings.py
+++ rsahda/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-ntj=q@8rt9z7m7gg1wwu8bi8$si6nlkb8wgg(mhu#u!1qhpvl^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'rsahda.herokuapp.com/']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'rsahda.herokuapp.com']
 
 
 # Application definition
