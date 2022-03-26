945793e7e9f3e391d8acf545a178b275fb83bc95
rishitv26 <75286874+rishitv26@users.noreply.github.com>
2022-03-25 22:17:28 -0400

    working like a mustard (adding updates) (ALMOST DONE!!!)

--- rsahda/settings.py
+++ rsahda/settings.py
@@ -128,6 +128,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
