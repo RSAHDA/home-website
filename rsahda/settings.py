8b8e64bf5ba9dbfaca01b337f49be13a47a91a78
rishitv26 <75286874+rishitv26@users.noreply.github.com>
2022-04-12 12:36:57 -0400

    done

--- rsahda/settings.py
+++ rsahda/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ntj=q@8rt9z7m7gg1wwu8bi8$si6nlkb8wgg(mhu#u!1qhpvl^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'rsahda.herokuapp.com']
 
