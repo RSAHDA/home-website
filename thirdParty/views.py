ead1cd74b8c774f498765bf2e0ba79b6064dd945
rishitv26 <75286874+rishitv26@users.noreply.github.com>
2022-05-07 11:44:53 -0400

    working

--- thirdParty/views.py
+++ thirdParty/views.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from django.shortcuts import render
 from . import models
 
 
@@ -8,13 +9,16 @@ def authenticator(request, id, host):
             application_host=host,
             application_id=id,
         )
-        # if it works, give user info:
-        return JsonResponse(
-            {"auth": True, "details": {
-                "username": request.user.username,
-                "first_name": request.user.first_name,
-                "last_name": request.user.last_name,
-            }})
+        # if it work; give user info:
+        if request.user.is_authenticated:
+            return JsonResponse(
+                {"auth": True, "details": {
+                    "username": request.user.username,
+                    "first_name": request.user.first_name,
+                    "last_name": request.user.last_name,
+                }}
+            )
+        else:
+            return JsonResponse({"auth": False})
     except:
-        return JsonResponse({"auth": False})
-
+        return render(request, "404.html")
