36781d45873c37ef0ee509211dccd6783c055463
Rishit Varshney <75286874+rishitv26@users.noreply.github.com>
2023-05-20 17:37:31 -0400

    bugfix for project

--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from github import *
 from .models import *
+import os
 
 
 def projects(request):
-    g = Github("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")
+    g = Github(os.environ.get("github_token"))
 
     for i in g.get_organization("RSAHDA").get_repos():
         project = Project(name=i.name, url=f"https://github.com/RSAHDA/{i.name}", description=i.description)
